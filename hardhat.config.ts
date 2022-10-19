97bc3975550b2813957b6e7fe6bdeba657ba1d47
iamcombo <piset2cambo@gmail.com>
2022-10-19 09:55:22 +0700

    - deploy smart contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config({ path: ".env" });
+
+const HTTP_URL = process.env.HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
+const POLYGON_SCAN_KEY = process.env.POLYGON_SCAN_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: HTTP_URL,
+      accounts: [PRIVATE_KEY],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
+  etherscan: {
+    apiKey: POLYGON_SCAN_KEY,
+  },
 };
 
 export default config;
